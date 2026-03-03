2fbd99aaf41a550e36c6acd6e228dfd380320ba0
tomiwaf <o.fasosin@gwu.edu>
2026-03-02 19:05:39 -0500

    Finished the Rotor class, all does what is expected

--- Rotor.java
+++ Rotor.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Rotor {
 
 
@@ -14,15 +16,42 @@ public class Rotor {
 
     public boolean rotate(){
         //TODO
+        char[] rotorV = this.rotorValues.toCharArray();
+        char[] rotorC = new char[rotorV.length];
+        char temp = rotorV[26];
+        for(int i = 1; i < rotorV.length; i++){
+            rotorC[i + 1] = rotorV[i];
+        }
+        rotorC[0] = temp;
+        this.rotorValues = Arrays.toString(rotorC);
+
+        if (temp == startChar) {
+            return true;
+        }
+        return false;
+
+
 
     }
 
 
     public int indexOf(char c){
         //TODO
+        char[] rotor = this.rotorValues.toCharArray();
+        for(int i = 0; i < rotor.length; i++){
+            if(rotor[i] == c){
+                return i;
+            }
+        }
+        return -1;
+
+
     }
 
     public char charAt(int idx){
         //TODO
+        char[] rotor = this.rotorValues.toCharArray();
+        return rotor[idx];
+
     }
 }
